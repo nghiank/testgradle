30bd3f60502011dcdd623bd76b83f09788aefd5b
iceman <nghiank@hotmail.com>
2014-06-17 11:35:38 +0800

    update

---
+++ src/java/main/code/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.code;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.print(RandomStringUtils.random(10));
+        System.out.print("Hello World!");
+    }
+}
